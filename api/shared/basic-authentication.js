a4805b37006032aa1820e3895dc88dac144bafab
mfaghfoory <meysam.tolive@gmail.com>
2020-11-22 16:18:48 +0330

    modified swagger 'id' parameter

--- api/shared/basic-authentication.js
+++ api/shared/basic-authentication.js
@@ -4,11 +4,13 @@ const pass = "test";
 module.exports = (req, res, next) => {
   if (
     !req.headers.authorization ||
-    req.headers.authorization.indexOf("basic") < 0
+    req.headers.authorization.toLowerCase().indexOf("basic") < 0
   ) {
     return res.status(401).send("you are not authenticated");
   }
-  const header = req.headers.authorization.replace("basic ", "");
+  const header = req.headers.authorization
+    .replace("basic ", "")
+    .replace("Basic ", "");
   const userPass = Buffer.from(header, "base64").toString("utf8").split(":");
   if (userPass.length < 2 || userPass[0] != user || userPass[1] != pass) {
     return res.status(401).send("authorization header is not valid");
